f1f65ebc9b46275e45c0c7771c9b0bd6c3013c9d
Rob Hess <hessro@oregonstate.edu>
2023-02-14 15:12:43 -0800

    Add code from 2/14 lecture.

--- app/src/main/java/com/example/multiactivitygithubsearch/RepoDetailActivity.kt
+++ app/src/main/java/com/example/multiactivitygithubsearch/RepoDetailActivity.kt
@@ -1,10 +1,15 @@
 package com.example.multiactivitygithubsearch
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import com.example.multiactivitygithubsearch.data.GitHubRepo
+import com.google.android.material.snackbar.Snackbar
 
 const val EXTRA_GITHUB_REPO = "GITHUB_REPO"
 
@@ -24,13 +29,49 @@ class RepoDetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun shareRepo() {
+        if (repo != null) {
+            val shareText = getString(R.string.share_text, repo!!.name, repo!!.url)
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, shareText)
+            startActivity(Intent.createChooser(intent, null))
+        }
+    }
+
     private fun viewOnWeb() {
         if (repo != null) {
+            val uri = Uri.parse(repo!!.url)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            try {
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Snackbar.make(
+                    findViewById(R.id.coordinator),
+                    "No app installed to handle that activity.  Please install a web browser.",
+                    Snackbar.LENGTH_LONG
+                )
+            }
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.repo_detail, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_view_on_web -> {
+                viewOnWeb()
+                true
+            }
+            R.id.action_share -> {
+                shareRepo()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
